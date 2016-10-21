fc7fff8f012f6906c3a0fe87def34515ddc30836
Soichiro-Yoshimura <yoshimura@soichiro.org>
2016-10-21 17:08:20 +0900

    Add INSERT

--- app/src/main/java/com/example/savingdata/MainActivity.java
+++ app/src/main/java/com/example/savingdata/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.savingdata;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -66,5 +68,38 @@ public class MainActivity extends AppCompatActivity {
         File file = getStorageDocumentsDir(filename);
         Log.d("MainActivity", "getFreeSpace: " + file.getFreeSpace());
         file.delete();
+    }
+
+    public void insertDb(View view) {
+        FeedReaderDbHelper mDbHelper = new FeedReaderDbHelper(this);
+        // Gets the data repository in write mode
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        // Create a new map of values, where column names are the keys
+        ContentValues values = new ContentValues();
+        long lastId = System.currentTimeMillis() / 1000;
+        values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_ENTRY_ID, lastId);
+        values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE, "タイトル");
+        values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_CONTENT, "内容");
+
+        // Insert the new row, returning the primary key value of the new row
+        long newRowId = db.insert(
+                FeedReaderContract.FeedEntry.TABLE_NAME,
+                "",
+                values);
+
+        Log.d("newRowId", Long.toString(newRowId));
+    }
+
+    public void selectDb(View view) {
+
+    }
+
+    public void deleteDb(View view) {
+
+    }
+
+    public void updateDb(View view) {
+
     }
 }
