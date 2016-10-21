e9964aef43a893c77431ae21bf6a4b60ebd2a141
Soichiro-Yoshimura <yoshimura@soichiro.org>
2016-10-21 16:05:19 +0900

    Add Internal Strage

--- app/src/main/java/com/example/savingdata/MainActivity.java
+++ app/src/main/java/com/example/savingdata/MainActivity.java
@@ -7,6 +7,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,18 +20,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    private final String filename = "myfile";
+
     public void save(View view) {
-        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putLong(getString(R.string.saved_time), System.currentTimeMillis());
-        editor.commit();
+        FileOutputStream outputStream;
+        try {
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            String string = "Hello Android! at " + System.currentTimeMillis();
+            outputStream.write(string.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void restore(View view) {
-        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
-        long savedTime = sharedPref.getLong(getString(R.string.saved_time), 0);
-        TextView textView = (TextView)findViewById(R.id.hello_text);
-        textView.setText(Long.toString(savedTime));
+        FileInputStream inputStream;
+        try {
+            inputStream = openFileInput(filename);
+            BufferedReader br =
+                    new BufferedReader(new InputStreamReader(inputStream));
+            StringBuilder sb = new StringBuilder();
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+            TextView textView = (TextView)findViewById(R.id.hello_text);
+            textView.setText(sb.toString());
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void delete(View view) {
