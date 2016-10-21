8291edd4b80ca0f22d19f669113eb1cf896a7959
Soichiro-Yoshimura <yoshimura@soichiro.org>
2016-10-21 14:55:09 +0900

    Add restore

--- app/src/main/java/com/example/savingdata/MainActivity.java
+++ app/src/main/java/com/example/savingdata/MainActivity.java
@@ -2,10 +2,10 @@ package com.example.savingdata;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +23,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void restore(View view) {
-
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        long savedTime = sharedPref.getLong(getString(R.string.saved_time), 0);
+        TextView textView = (TextView)findViewById(R.id.hello_text);
+        textView.setText(Long.toString(savedTime));
     }
 
     public void delete(View view) {
